9999fd3051983efb849265126f0bad795d205168
mibaj <mibaj@dtu.dk>
2022-03-01 18:02:13 +0100

    u

--- utils/analysis.py
+++ utils/analysis.py
@@ -308,7 +308,7 @@ def find_optimal_subset(X, y, valid_indices = None, n_trees=500, fmax = None, re
     
         # Feature search
         tscv = TimeSeriesSplit(n_splits=5)
-        fmax = 30 #remove
+        fmax = 20 #remove
         if not fmax:
             fmax = X.shape[1]-1
             
