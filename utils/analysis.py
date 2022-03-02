b39da0132d2517233b1148372aecede702387210
mibaj <mibaj@dtu.dk>
2022-03-02 20:41:25 +0100

    u

--- utils/analysis.py
+++ utils/analysis.py
@@ -316,7 +316,7 @@ def find_optimal_subset(X, y, valid_indices = None, n_trees=1000, fmax = None, r
         if reg_model:
             f=(1,fmax) 
             #model = RandomForestRegressor(n_trees, min_impurity_decrease=0.015, max_depth = 10, min_samples_leaf = 2)
-            model = KNeighborsRegressor(n_neighbors=100)
+            model = KNeighborsRegressor(n_neighbors=500)
             if valid_indices is not None:
                 print('Using a valid subset')
                 valid_subset = PredefinedHoldoutSplit(valid_indices)
@@ -324,7 +324,7 @@ def find_optimal_subset(X, y, valid_indices = None, n_trees=1000, fmax = None, r
                                                                                forward=True,
                                                                                verbose=4,
                                                                                scoring='r2',
-                                                                               cv=10)
+                                                                               cv=20)
                                                                                #cv = valid_subset)
                                                                               
                              
