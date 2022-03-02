4f2e9a104bdc4ab5f9edce4de4e9f66ba6533f0e
mibaj <mibaj@dtu.dk>
2022-03-02 14:56:04 +0100

    u

--- utils/analysis.py
+++ utils/analysis.py
@@ -317,7 +317,7 @@ def find_optimal_subset(X, y, valid_indices = None, n_trees=500, fmax = None, re
             if valid_indices is not None:
                 print('Using a valid subset')
                 valid_subset = PredefinedHoldoutSplit(valid_indices)
-                feature_selector = SequentialFeatureSelector(RandomForestRegressor(n_trees, min_impurity_decrease=0.001,  min_samples_leaf = 2), 
+                feature_selector = SequentialFeatureSelector(RandomForestRegressor(n_trees, min_impurity_decrease=0.01,  min_samples_leaf = 2), 
                                                                                n_jobs=-1,
                                                                                k_features=f,
                                                                                forward=True,
